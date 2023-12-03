0e565da476723589dfe7dc480b70c3f4e060c4bb
liufuxiang <842110796@qq.com>
2023-12-03 18:10:46 +0800

    feat(init): 初始化项目

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <i-ep-aim />
+    <i-ep-plus />
+  </div>
+</template>
+
+<style>
+@media (width >= 1024px) {
+  .about {
+    display: flex;
+    align-items: center;
+    min-height: 100vh;
+  }
+}
+</style>
